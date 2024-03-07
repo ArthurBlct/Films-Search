4c135e02f8b3fa5738a0ce5d528287fa474532a6
ArthurBlct <124893220+ArthurBlct@users.noreply.github.com>
2024-03-07 17:43:12 +0100

    display list fix

--- script.js
+++ script.js
@@ -4,22 +4,18 @@ const key = "6a7eaccc"
 const search = document.getElementById('search')
 const input = document.getElementById('input')
 const prefs = document.getElementById('preferences')
+const list = document.querySelector('.list')
 
 search.addEventListener('click', () => {
     if (input.value != "") {
+        list.innerHTML = ""
         const url = `https://www.omdbapi.com/?apikey=${key}&s=${input.value}`
         fetch (url)
             .then(res => res.json())
             .then(data => {
                 console.log(data)
-                // console.log(data.totalResults)
                 
                 input.value = ""
-                // console.log('displayMovies')
-                
-                // for (movie in data) {
-                //     console.log(data.Title)
-                // }
 
                 for (let i = 0; i < data.Search.length; i++) {
                     const movie = data.Search[i]
@@ -27,18 +23,14 @@ search.addEventListener('click', () => {
                     const div = document.createElement('div')
                     div.innerHTML = ` 
                         <img class="poster" src="${movie.Poster}" alt="${movie.Title}">
-                        <h1>${movie.Title}</h1>
+                        <h2>${movie.Title}</h2>
                         <h2>${movie.Year}</h2>
                         <h3>${movie.Type}</h3>
                         <button>Ajouter</button>
                     `
-                    prefs.appendChild(div)
+                    div.classList.add('movie')
+                    list.appendChild(div)
                 }
-
-
-
-
-
 
             })
             .catch(e => console.error(e))
